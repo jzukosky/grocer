122a467d97df8cefd91784377b7fbcf703cea53d
qgvc4 <qgvc4@mail.missouri.edu>
2018-10-22 17:33:55 -0500

    init xcode project

---
+++ Grocer/Grocer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grocer
+//
+//  Created by Qiwen Guo on 10/22/18.
+//  Copyright © 2018 it4500. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
