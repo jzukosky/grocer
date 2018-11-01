37399b20884076fe74a74321378f58810ea4f17b
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2018-10-31 19:10:44 -0500

    Added basic buttons, no functionality yet.

---
+++ Grocer/Views/UsersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsersViewController.swift
+//  Grocer
+//
+//  Created by Brendan Krekeler on 10/31/18.
+//  Copyright © 2018 it4500. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
