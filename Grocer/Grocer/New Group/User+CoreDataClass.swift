e55947548f4d52e74b7e10728f4d06091242f118
MelissaHollingshed <mahv87@mail.missouri.edu>
2018-11-06 10:22:35 -0600

    added photo control

---
+++ Grocer/Grocer/New Group/User+CoreDataClass.swift
@@ -0,0 +1,28 @@
+//
+//  User+CoreDataClass.swift
+//  Grocer
+//
+//  Created by linChunbin on 10/29/18.
+//  Copyright © 2018 it4500. All rights reserved.
+//
+//
+
+import UIKit
+import CoreData
+
+public class User: NSManagedObject {
+
+    convenience init?(username: String, email:String?, information:String?, picture: NSData?){
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return nil
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        self.init(entity: User.entity(), insertInto: context)
+        self.username = username
+        self.email = email
+        self.information = information
+        self.picture = picture
+    }
+}
--- Grocer/Views/EditUsersViewController.swift
+++ Grocer/Views/EditUsersViewController.swift
@@ -8,15 +8,73 @@
 
 import UIKit
 
-class EditUsersViewController: UIViewController {
 
+class EditUsersViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet weak var userPictureImageView: UIImageView!
+    @IBOutlet weak var informationTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBAction func chooseUserPictureButtonTapped(_ sender: Any) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = true
+        self.present(image, animated: true)
+        {
+            
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+            userPictureImageView.image = image
+        }
+        else
+        {
+                //error
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    var existingUser: User?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
     
 
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let username = userNameTextField.text else {
+            return
+        }
+        var user: User?
+        if let existingUser = existingUser {
+            user = existingUser
+            user?.username = username
+            user?.information = informationTextField.text
+            user?.email = emailTextField.text
+        
+        } else {
+           // user = User(username: username, email: emailTextField.text, information: informationTextField.text, picture: userPictureImageView)
+           
+        }
+        do {
+            try user?.managedObjectContext?.save()
+            navigationController?.popViewController(animated: true)
+        } catch {
+            print("Failed to save user")
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
