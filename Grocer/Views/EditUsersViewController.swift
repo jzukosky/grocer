fa69cd2b96af6cab4b991d4bebc5e0cc19c71db7
MelissaHollingshed <mahv87@mail.missouri.edu>
2018-11-05 20:31:20 -0600

    Added UI

---
+++ Grocer/Views/EditUsersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditUsersViewController.swift
+//  Grocer
+//
+//  Created by Melissa Hollingshed on 11/5/18.
+//  Copyright © 2018 it4500. All rights reserved.
+//
+
+import UIKit
+
+class EditUsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
