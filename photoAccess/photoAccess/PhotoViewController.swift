1def16b890b6362b06ef28b8703f4b84eaad9fe4
Roger Kiew <youchiu416@gmail.com>
2018-11-08 13:42:24 -0600

    add some codes and not done yet

--- photoAccess/photoAccess/PhotoViewController.swift
+++ photoAccess/photoAccess/PhotoViewController.swift
@@ -11,6 +11,8 @@ import AVFoundation
 import Photos
 
 class PhotoViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,45 +21,58 @@ class PhotoViewController: UIViewController,UIImagePickerControllerDelegate, UIN
     }
     
     @IBAction func requestCameraButton(_ sender: UIButton) {
-        
+        AVCaptureDevice.requestAccess(for: .video, completionHandler: {accessGranted in
+            guard accessGranted == true else { return }
+            self.presentCamera()
+        })
         let camera = AVCaptureDevice.authorizationStatus(for: .video)
-       
-            AVCaptureDevice.requestAccess(for: .video, completionHandler: {accessGranted in
-                guard accessGranted == true else { return }
-                self.presentCamera()
-            })
-         if camera != .authorized{
+        
+        if camera == .denied{
             let alert = UIAlertController(title: "We need your permisson", message: "Please go to settings to allow camera permission", preferredStyle: .alert)
             
             alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
-            
             self.present(alert, animated: true)
         }
     }
     @IBAction func requestPhotoLibraryButton(_ sender: UIButton) {
         let photos = PHPhotoLibrary.authorizationStatus()
-            PHPhotoLibrary.requestAuthorization({status in
-                if status == .authorized{
-                    self.presentPhoto()
-                } else {}
-            })
-        
-        if photos != .authorized{
-            let alert = UIAlertController(title: "We need your permisson", message: "Please go to settings to allow photo library permission", preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
-            
-            self.present(alert, animated: true)
-        }
+ 
+        PHPhotoLibrary.requestAuthorization({status in
+            if status == .authorized{
+                self.presentPhoto()
+            } else {
+                let alert = UIAlertController(title: "We need your permisson", message: "Please go to settings to allow photo library permission", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+                self.present(alert, animated: true)
+            }
+        })
     }
     func presentPhoto(){
         
         print("Photo library success")
-    }
+        
+        DispatchQueue.main.async {
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                self.imagePicker.delegate = self
+                self.imagePicker.sourceType = .photoLibrary;
+                self.imagePicker.allowsEditing = false
+                
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }}
+        }
     func presentCamera(){
         print("camera success")
         
     }
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismiss(animated: true, completion: { () -> Void in
+            
+        })
+        print("select")
+        imageView.image = image
+        
+    }
+    
     /*
     // MARK: - Navigation
 
