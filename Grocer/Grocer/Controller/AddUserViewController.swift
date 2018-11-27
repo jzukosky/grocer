3e877474df5d20da1817e671d387b6ff39ddb165
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2018-11-27 16:30:20 -0600

    added code for functionality, waiting for core data.

--- Grocer/Grocer/Controller/AddUserViewController.swift
+++ Grocer/Grocer/Controller/AddUserViewController.swift
@@ -8,23 +8,68 @@
 
 import UIKit
 
-class AddUserViewController: UIViewController {
+class AddUserViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var userPictureImageView: UIImageView!
+    @IBOutlet weak var informationTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    var newUser: User?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func chooseUserPictureButtonTapped(_ sender: Any) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = true
+        self.present(image, animated: true)
+        {
+            
+        }
     }
-    */
-
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let username = userNameTextField.text else {
+            return
+        }
+        var user: User?
+        if let existingUser = newUser {
+            user = existingUser
+            user?.username = username
+            user?.information = informationTextField.text
+            user?.email = emailTextField.text
+            
+        } else {
+            //user = User(username: username, email: emailTextField.text, information: informationTextField.text, picture: userPictureImageView)
+            
+        }
+        do {
+            try user?.managedObjectContext?.save()
+            navigationController?.popViewController(animated: true)
+        } catch {
+            print("Failed to save user")
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+            userPictureImageView.image = image
+        }
+        else
+        {
+            //error
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 }
