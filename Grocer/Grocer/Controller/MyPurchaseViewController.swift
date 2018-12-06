99d609341d41dec66b346a8fc133bd4eacf35508
lin chunbin <Ben@linchunbindemacbook-pro.local>
2018-12-06 13:55:51 -0600

    Added calculation

--- Grocer/Grocer/Controller/MyPurchaseViewController.swift
+++ Grocer/Grocer/Controller/MyPurchaseViewController.swift
@@ -7,44 +7,84 @@
 //
 
 import UIKit
+import CoreData
 
 class MyPurchaseViewController: UIViewController {
     
+    var payment: Double?
     var myItems: [Item] = []
     var user: User?
+    var itemCount : Int?
     @IBOutlet weak var priceLabel: UILabel!
     
     @IBOutlet weak var sendEmailButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let user = user{
-            for item in myItems{
-                print(item.getName())
-                print(item.getPrice())
-                print(user.getUsername())
-                item.addToUsers(user)
+        if let fetchedUsers = fetchUsers(){
+            for tempUser in fetchedUsers{
+                if tempUser.isEqual(user){
+                    if let fetchedItems = tempUser.getItems(){
+                        itemCount = fetchedItems.count
+                    }
+                }
             }
-            
-            do{
-                let managedObjectContext = user.managedObjectContext
-                try managedObjectContext?.save()
-            }catch{
-                print("there is an error during save data\(error)")
-                return
-            }
-            
-            print("Items saved to user")
         }
         
+        if itemCount != myItems.count{
+            if let user = user{
+                for item in myItems{
+                    print(item.getName())
+                    print(item.getPrice())
+                    print(user.getUsername())
+                    item.addToUsers(user)
+                }
+                
+                do{
+                    let managedObjectContext = user.managedObjectContext
+                    try managedObjectContext?.save()
+                }catch{
+                    print("there is an error during save data\(error)")
+                    return
+                }
+                
+                print("Items saved to user")
+            }
+        }
+        
+        calculatePayment()
         
         // Do any additional setup after loading the view.
     }
     
 
     func calculatePayment(){
+        var payment = 0.0
         
         
+        for item in myItems{
+            if let userCount = item.getUsers()?.count{
+                payment += (item.price) / Double(userCount)
+            }
+        }
+        priceLabel.text = "$ \(payment)"
+    }
+    
+    func fetchUsers()->[User]?{
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return nil
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<User> = User.fetchRequest()
+        var fetchedUsers: [User] = []
+        do {
+            fetchedUsers = try managedContext.fetch(fetchRequest)
+        } catch {
+            print("Error fetch user)")
+        }
+        return fetchedUsers
     }
     /*
     // MARK: - Navigation
