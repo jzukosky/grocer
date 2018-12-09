34eabdf6032789c43e82f7904af2295ce51c2c4c
Roger Kiew <youchiu416@gmail.com>
2018-12-09 14:58:08 -0600

    undo the change, now only send eamil to the current user

--- Grocer/Grocer/Controller/MyPurchaseViewController.swift
+++ Grocer/Grocer/Controller/MyPurchaseViewController.swift
@@ -139,12 +139,8 @@ class MyPurchaseViewController: UIViewController, MFMailComposeViewControllerDel
     func configureMailController() -> MFMailComposeViewController {
         let mailComposerVC = MFMailComposeViewController()
         mailComposerVC.mailComposeDelegate = self
-        var users = fetchUsers()
-        var emails: [String] = []
-        for user in users!{
-            emails.append(user.getEmail()!)
-        }
-        mailComposerVC.setToRecipients(emails)
+        var email = user?.getEmail()
+        mailComposerVC.setToRecipients([email!])
         mailComposerVC.setSubject("Pay your stuff")
         mailComposerVC.setMessageBody("Please pay the amount: $\(payment)", isHTML: false)
         
